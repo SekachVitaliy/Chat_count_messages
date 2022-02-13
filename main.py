89d09f161f6b37998c835218e2cf3a4ee7440f97
Vitaliy Sekach <50178320+SekachVitaliy@users.noreply.github.com>
2022-02-13 15:57:50 +0200

    add function for get your dialogs

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import asyncio
+from config import API_ID, API_HASH
 from pyrogram import Client
 
+chat_id = 000000000
+
+
+def get_dialogs():
+    with Client("my_account", API_ID, API_HASH) as app:
+        for dialog in app.iter_dialogs():
+            name_data = [dialog.chat.first_name, dialog.chat.last_name,
+                         dialog.chat.username, dialog.chat.title]
+            name = ' '.join([i for i in name_data if i is not None])
+            print(f"{name=}, {dialog.chat.id=}")
+
+
 if __name__ == "__main__":
-    pass
+    get_dialogs()
