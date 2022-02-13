b2459e316880210e94c58baedaf5e42823e94aa7
Vitaliy Sekach <50178320+SekachVitaliy@users.noreply.github.com>
2022-02-13 15:18:18 +0200

    add config and get api_keys from env

---
+++ config.py
@@ -0,0 +1,6 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+API_ID = env.str('API_ID')
+API_HASH = env.str('API_HASH')
---
+++ main.py
@@ -0,0 +1,5 @@
+import asyncio
+from pyrogram import Client
+
+if __name__ == "__main__":
+    pass
